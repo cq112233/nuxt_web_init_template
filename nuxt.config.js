6656bf068bf7eebe7033f4d65a86b4c20e04b483
cq <1549914423@qq.com>
2021-12-26 22:23:47 +0800

    feat:部署

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import pageRoutes from "./router/pageRoutes";
 export default {
   server:{
     host: "0.0.0.0",
-    port: 3000
+    port: 3006
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
